682c9365b595c5990923d29683928d79ce07857e
vDeee092 <vd.puroy@gmail.com>
2026-03-26 23:58:38 -0700

    feat: expandable menu when click Change Game button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Button, Stack, Divider} from '@mui/material';
+import { Button, Stack, Divider, Collapse} from '@mui/material';
 import { useState } from "react";
 
 const Navbar = () => {
@@ -6,6 +6,10 @@ const Navbar = () => {
     return (
         <Stack sx={{flex: 1.5, justifyContent:{sm:'center'}}}>
             <Button variant="text" onClick={() => setShowGame(!showGame)}>Change Game</Button>
+            <Collapse in={showGame}>
+              <Button fullWidth variant="text">Guess the Rating</Button>
+              <Button fullWidth variant="text">Matching Game</Button>
+            </Collapse>
             <Divider />
             <Button variant="text">Leaderboard</Button>
         </Stack>
